5206ff5f361f1f495550b184c8019ccb7ac17daf
Saikat Dey <saikatdey2100@gmail.com>
2023-10-09 22:38:37 +0530

    Docker Setup & Initials Tests.
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

---
+++ digonto.in/web/__tests__/index.test.ts
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Dummy Test Suite", () => {
+  it("Dummy Test", () => {
+    const val = true;
+    expect(val).toBeTruthy();
+  });
+});
