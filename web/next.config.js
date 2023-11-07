93bd0430d4aab211effc20774e946e59bd7ddac4
Saikat Dey <saikatdey2100@gmail.com>
2023-11-07 06:04:23 +0530

    Fixed production webpack module path alias.
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+const nextConfig = {
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
