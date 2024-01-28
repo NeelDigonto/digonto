f1e0e0a68f3c4b69016ade0315dd2555bb80646c
Saikat Dey <saikatdey2100@gmail.com>
2024-01-29 03:51:10 +0530

    add support for glsl
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,20 @@
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+
+    config.module.rules.push({
+      test: /\.(frag|vert|glsl)$/,
+      type: "asset/source",
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Test from "@/components/Test";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Test/>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
